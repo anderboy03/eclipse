cf73788c90133db2a9695968b21f724cfdcefa4a
Anderson Clayton Ribas <andersex03@gmail.com>
2023-02-15 14:54:09 -0300

    integração com IDE eclipse com github

---
+++ hello github/src/br/anderson/git/hellogit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.anderson.git;
+
+/**
+ * @author Ander
+ *Exemplo de integração do eclipse com github
+ */
+public class hellogit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello git");
+
+	}
+
+}
